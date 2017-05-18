953eef403cb03ad425a44e691010db622494021a
thoni <running@thomas-niebler.de>
2017-05-18 16:25:54 +0200

    added embeddings, updated code

--- src/semmele/example_call.py
+++ src/semmele/example_call.py
@@ -3,15 +3,15 @@ from semmele.utils import load_eval_df, load_vecs
 from .lsml import train_until_completed
 from .utils import prepare_dataset
 
-path_to_vecfile = "/home/niebler/work/semanticmetriclearning/data/wikiglove"
+path_to_vecfile = "embeddings/delicious/vec_complete_file_glove_dim100"
 
-ws353 = load_eval_df("ws353")
+men = load_eval_df("men")
 vectors = load_vecs(path_to_vecfile)
 
 # find vectors for the words in the evaldf
-ws353df = prepare_dataset(ws353, vectors)
+mendf = prepare_dataset(men, vectors)
 
-train_data, test_data = get_train_test(ws353df, samples=int(0.8 * len(ws353df)))
+train_data, test_data = get_train_test(mendf, samples=int(0.8 * len(mendf)))
 
 for training_samples in [int(i * len(train_data)) for i in range(0.1, 1.1, 0.1)]:
     result_scores, result_metrics = train_until_completed(train_data, training_samples, test_data)
