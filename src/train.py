fc7f47912cbebb66e31d9a2e7d296f40947b4b80
thoni <running@thomas-niebler.de>
2018-02-05 21:07:23 +0100

    fiddling around with learning rate submission

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser(description="Retrain Word Embeddings using Rela
 parser.add_argument('embeddings', help="word embedding text file. file format is \"name val1 val2 val3 ...\"")
 parser.add_argument('relscores', help="a tab separated csv file with word pairs and numeric relatedness scores, columns"
                                       " headers must be in the first line")
-parser.add_argument('-l', '--learningrate', type=float, default=None, help="learning rate")
+parser.add_argument('-l', '--learningrate', help="learning rate")
 parser.add_argument('-v', '--verbose', action="store_true", help="more elaborate output")
 parser.add_argument('--outputfile', help="path where the transformed vectors should be saved to")
 parser.add_argument('--evalsteps', action="store_true",
@@ -25,7 +25,7 @@ relscores["relatedness"] = relscores["relatedness"].apply(float)
 
 alg = rrl.RRL(verbose=args.verbose)
 print(str(datetime.now()) + "\tTraining...")
-model = alg.fit(vectors, relscores, eval_steps=args.evalsteps, step_sizes=[args.learningrate])
+model = alg.fit(vectors, relscores, eval_steps=args.evalsteps, step_sizes=args.learningrate)
 
 for met in model._Ms:
     print(
