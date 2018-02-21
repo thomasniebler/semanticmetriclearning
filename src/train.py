ae8a1cf47d76130a81d23054af9a866d8ef9fdfd
thoni <running@thomas-niebler.de>
2018-02-21 17:35:33 +0100

    added separate dirs and files for output

--- src/train.py
+++ src/train.py
@@ -17,11 +17,12 @@ parser.add_argument('-a', '--learningrateadaption', type=int,
                     default=1)
 parser.add_argument('-b', '--batchsize', type=int, default=100, help="batchsize")
 parser.add_argument('-v', '--verbose', action="store_true", help="more elaborate output")
-parser.add_argument('-o', '--outputdir', help="path where the transformed vectors should be saved to")
+parser.add_argument('-o', '--outputfile', help="path where the transformed vectors should be saved to")
 parser.add_argument('-c', '--maxsparkcores', type=int, default=30,
                     help="maximum number of cores that gradient computation can use")
 parser.add_argument('-e', '--evalsteps', action="store_true",
                     help="show evaluation scores on a range of datasets after every iteration")
+parser.add_argument('--evalstepsdir', help="directory where steps are saved to")
 parser.add_argument('-p', '--epochs', type=int, default=100, help="Number of training epochs")
 args = parser.parse_args()
 
@@ -34,15 +35,15 @@ relscores["relatedness"] = relscores["relatedness"].apply(float)
 alg = rrl.RRL(verbose=args.verbose, epochs=args.epochs)
 print(str(datetime.now()) + "\tTraining...")
 model = alg.fit(vectors, relscores, eval_steps=args.evalsteps, learning_rate=args.learningrate,
-                batchsize=args.batchsize, output_dir=args.outputdir, max_spark_cores=args.maxsparkcores,
+                batchsize=args.batchsize, max_spark_cores=args.maxsparkcores, output_dir=args.evalstepsdir,
                 learning_rate_adaption=args.learningrateadaption)
 
 print(str(datetime.now()) + "\tTransforming vectors...")
 transformedvecs = model.transform()
-if args.outputdir is not None and args.outputdir[-1] != "/":
-    args.outputdir += "/"
-outputfile = open(
-    args.inputfile + "_rrl_" + str(args.learningrate) + "_" + str(args.batchsize), "w")
+# if args.outputfile is not None and args.outputfile[-1] != "/":
+#    args.outputfile += "/"
+outputfile = open(args.outputfile, "w")
+#    args.inputfile + "_rrl_" + str(args.learningrate) + "_" + str(args.batchsize), "w")
 for namevec in transformedvecs.items():
     outputfile.write(namevec[0] + " " + " ".join(map(str, map(lambda x: round(x, 3), namevec[1]))) + "\n")
 
